0aa58c293a67a5ccdae7885b0111aa2d9fca7dea
ZzDangprozZ3 <lehongdang2k4@gmail.com>
2025-11-20 13:19:16 +0100

    prediction commented

--- prediction.py
+++ prediction.py
@@ -10,22 +10,22 @@ from PIL import Image
 import json
 import re
 
-def numerical_sort(value):
+def numerical_sort(value): # Fonction de tri numérique des noms de fichiers
     numbers = re.findall(r'\d+', value)
     return int(numbers[0]) if numbers else 0
 
 transform = transforms.Compose([
-   transforms.Resize((224, 224)),   # B0 chuẩn 224x224
+   transforms.Resize((224, 224)),  # Taille d'entrée fixe pour EfficientNet-B0
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                          std=[0.229, 0.224, 0.225])
 ])
-  # đường dẫn tới folder test
+# Définir le Dataset pour le dossier de test
 class TestDataset(Dataset):
     def __init__(self, img_dir, transform=None):
         self.img_dir = img_dir
         self.transform = transform
-        self.img_names = sorted(os.listdir(img_dir), key=numerical_sort)  # giữ thứ tự để JSON index đúng
+        self.img_names = sorted(os.listdir(img_dir), key=numerical_sort)  # Tri numérique des noms de fichiers
 
     def __len__(self):
         return len(self.img_names)
@@ -38,26 +38,23 @@ class TestDataset(Dataset):
             image = self.transform(image)
         return image, img_name
     
-test_dir = "test"
+test_dir = "test"  # Dossier de test, faut modifier si nécessaire
 test_dataset = TestDataset(test_dir, transform=transform)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
-# img_path = "test/420.jpg"  # thay bằng tên file ảnh
-# image = Image.open(img_path).convert("RGB")
-# image = transform(image).unsqueeze(0)  # thêm batch dim
-
-
+test_loader = DataLoader(test_dataset, batch_size=10, shuffle=False)
 
 if __name__ == '__main__':
-    device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+    device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu" # S'assurer d'utiliser le bon device
     print(device)
     classes = ['bread', 'dairy', 'dessert', 'egg', 'fried', 'meat', 'noodles-pasta', 'rice', 'seafood', 'soup', 'vegetable-fruit']
     pred_dict = {}
+    # Load model and set to eval mode, on utilise EfficientNet-B0 car c'est plus performant
     model = efficientnet_b0()
     model.classifier[1] = nn.Linear(model.classifier[1].in_features, 11)
     model.load_state_dict(torch.load("modelCNN.pth", weights_only=True))
     model.to(device)
     model.eval()
-    with torch.no_grad():
+
+    with torch.no_grad(): # Commencer les prédictions
         for images, img_names in test_loader:
             images = images.to(device)
             outputs = model(images)
@@ -66,9 +63,9 @@ if __name__ == '__main__':
                 idx = test_dataset.img_names.index(img_name)  # giữ thứ tự index
                 pred_dict[str(idx)] = classes[preds[i]]
     # -------------------------
-    # 6️⃣ Lưu ra JSON
+    # Sauvegarder en JSON
     # -------------------------
-    with open("test_predictions.json", "w") as f:
+    with open("predictions.json", "w") as f:
         json.dump(pred_dict, f, indent=4)
 
     print("Done! JSON file created.")
